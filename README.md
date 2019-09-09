06da4b4b7d3ccc4cd5540974a9f833998b0d64e6
arwn <windham.aren@protonmail.com>
2019-09-09 16:17:33 -0700

    update readme, add api stuff

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+# hypertube
 Basically..
--- hypertube.go
+++ hypertube.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "<h1>Hello from index</h1>")
+}
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello from %s", mux.Vars(r)["world"])
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "\"%s\" not found", r.URL)
+}
+
 func main() {
-  print("fuck")
+	router := mux.NewRouter()
+	router.HandleFunc("/", indexHandler)
+	router.HandleFunc("/hello/{world}", helloWorldHandler)
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	fmt.Println("Starting server on port 8800")
+	log.Fatal(http.ListenAndServe(":8800", router))
 }
