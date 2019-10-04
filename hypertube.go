479061dabf9a4cf7cec20d8b212c91943021695a
Alexander <bombblob@gmail.com>
2019-10-03 18:49:39 -0700

    Added oauth redirect handler

--- hypertube.go
+++ hypertube.go
@@ -61,6 +61,46 @@ handleErr:
 	fmt.Fprint(w, err)
 }
 
+type tokenJSON struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	RefreshToken string `json:"refresh_token"`
+	ExpiresIn    int32  `json:"expires_in"`
+}
+
+// request should look something like:
+// https://github.com/login/oauth/authorize?client_id=CLIENT_ID&redirect_uri=http://localhost:8800/oauth/redirect
+func oauthRedirectGithubHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		panic(err) // TODO: address error
+	}
+	code := r.FormValue("code")
+	reqURL := fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s", clientID_github, clientSecret_github, code)
+	req, err := http.NewRequest(http.MethodPost, reqURL, nil)
+	if err != nil {
+		panic(err) // TODO: address error
+	}
+	req.Header.Set("accept", "application/json")
+
+	httpClient := http.Client{}
+	res, err := httpClient.Do(req)
+	if err != nil {
+		panic(err) // TODO: address error
+	}
+	defer res.Body.Close()
+
+	var t tokenJSON
+	if err := json.NewDecoder(res.Body).Decode(&t); err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Location", "/")
+	w.WriteHeader(http.StatusFound)
+	io.WriteString(w, t.AccessToken)
+	// TODO: do something with AccessToken
+}
+
 func userLoginHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	fmt.Fprintf(w, "Not implemented yet\n\nLogging in `%s`", vars["username"])
@@ -207,6 +247,7 @@ func videoWatchHandlerTest(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// split `buf` into byte slices of at most `size` size
 func splitBytes(buf []byte, size int) [][]byte {
 	chunks := [][]byte{}
 	for len(buf) >= size {
@@ -250,6 +291,9 @@ func videoWatchHandlerHash(w http.ResponseWriter, r *http.Request) {
 func createRoutes(router *mux.Router) {
 	router.HandleFunc("/", indexHandler)
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	router.HandleFunc("/oauth/redirect/github", oauthRedirectGithubHandler)
+
 	router.HandleFunc("/api/user/", userUsageHandler)
 	router.HandleFunc("/api/user/create/", userCreateHandler)
 	router.HandleFunc("/api/user/login/{username}", userLoginHandler)
@@ -257,6 +301,7 @@ func createRoutes(router *mux.Router) {
 	router.HandleFunc("/api/user/update/{username}", userUpdateHandler)
 	router.HandleFunc("/api/user/delete/{username}", userDeleteHandler)
 	router.HandleFunc("/api/user/info/{username}", userInfoHandler)
+
 	router.HandleFunc("/api/video/info/{uuid}", videoInfoHandler)
 	router.HandleFunc("/api/video/create/", videoCreateHandler)
 	router.HandleFunc("/api/video/delete/{uuid}", videoDeleteHandler)
@@ -267,9 +312,15 @@ func createRoutes(router *mux.Router) {
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 }
 
+var clientID_github = ""
+var clientSecret_github = ""
+
 func main() {
 	router := mux.NewRouter()
 	createRoutes(router)
+
+	clientID_github = os.Getenv("clientID_github")
+	clientSecret_github = os.Getenv("clientSecret_github")
 
 	DBInit("postgres")
 	DBGenerateTablesPrompt()
